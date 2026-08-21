517db32ee5d0067ac85e3e2e908407e076c3d68b
wiliami <wiliaup97@gmail.com>
2026-08-21 00:50:39 -0400

    chore: add code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import './App.css'
 
-
-interface ButtonProps {
-  texto: string
-}
-
-function Button(props: ButtonProps) {
-  return <button>{props.texto}</button>
-}
-
 export default function App() {
-
   return (
-    <div>
-      <Button texto='Clique aqui' />
-      <Button texto='Texto 2'/>
-      <Button texto='Texto de número 3' />
-    </div>
+    <h1>I'm a hacker 💀</h1> 
   )
 } 
